587ce1e56cf2587f62f5d52a91cab20417269cc9
Mazene <mmazenezerguine@gmail.com>
2022-06-28 17:22:22 +0200

    end

--- app/src/main/java/com/example/qrcode_app/ScanActivity.java
+++ app/src/main/java/com/example/qrcode_app/ScanActivity.java
@@ -1,14 +1,48 @@
 package com.example.qrcode_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.google.zxing.Result;
 
 public class ScanActivity extends AppCompatActivity {
+
+    private CodeScanner mCodeScanner;
+
+    TextView text ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan);
+
+        text = findViewById(R.id.text_display) ;
+        CodeScannerView scannerView = findViewById(R.id.scanner_view);
+        mCodeScanner = new CodeScanner(this, scannerView);
+
+        mCodeScanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull final Result result) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        String value = result.getText().toString() ;
+                        text.setText(value);
+                     }
+                });
+            }
+        });
+
+
     }
+
 }
